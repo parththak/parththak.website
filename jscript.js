da6dfc27beab58cde573aaefa6f802202b21bc2d
Parth Thakkar <pthakkar100@gmail.com>
2019-11-17 23:17:03 -0500

    fixed up scroll button

--- jscript.js
+++ jscript.js
@@ -1,19 +1,11 @@
 $(document).ready(function(){
-    $("#project-scroller").click(function() {
-        $('html,body').animate({
-            scrollTop: $(".projects").offset().top},
-            100);
-    });
-  
-});
-
-$(document).ready(function(){
+    
     $("#exp-scroller").click(function() {
         $('html,body').animate({
             scrollTop: $(".experience").offset().top},
             100);
     });
-  
 });
+
 
 
