e42cfa66d9e7f47b39bd117845d13ed0add4b198
Parth Thakkar <pthakkar100@gmail.com>
2019-11-17 19:27:36 -0500

    Added new file structure and experiences section

--- jscript.js
+++ jscript.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
             scrollTop: $(".projects").offset().top},
             100);
     });
+  
 });
+
+$(document).ready(function(){
+    $("#exp-scroller").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".experience").offset().top},
+            100);
+    });
+  
+});
+
+
