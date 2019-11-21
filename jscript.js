e49e6c6456a0538b1e4c7a98a84dad2d5f8d6859
Parth <pthakkar100@gmail.com>
2019-11-21 12:04:15 -0500

    Scroll button now sticks to bottom of the page
    Moved nav-scroll into profile section for scaling

--- jscript.js
+++ jscript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
     $("#exp-scroller").click(function() {
         $('html,body').animate({
-            scrollTop: $(".project-section").offset().top},
+            scrollTop: $(".exp-section").offset().top},
             100);
     });
 });
